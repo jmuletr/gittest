d89081eb75f0b5ad76a041bfe43959e55980ea53
jmuletr <jmuletr@gmail.com>
2016-11-30 21:14:40 +0100

    classe persona

---
+++ src/persona.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jmuletr on 23/11/16.
+ */
+public class persona {
+    private String dni;
+    private String nom;
+    private String cognom;
+
+    public persona() {
+
+    }
+}
